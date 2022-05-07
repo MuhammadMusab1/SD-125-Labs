850c078742c36dbc412f32bc583c255df5467523
musab <yaqoobmusab03@gmail.com>
2022-05-06 21:50:34 -0500

    Lab 3 files

---
+++ ParkoMatikDB/ParkoMatikDB/Models/Database.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ParkoMatikDB.Models
+{
+    public class Vehicle
+    {
+        public int ID { get; set; }
+        public int PassID { get; set; }
+        public virtual Pass Pass { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
+        public bool Parked { get; set; }
+    }
+
+    public class Pass //capacity determines how many vehicles can be list up for parking
+    {
+        public int ID { get; set; }
+        public string Purchaser { get; set; }
+        public bool Premium { get; set; }
+        public int Capacity { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+
+    }
+
+    public class ParkingSpot
+    {
+        public int ID { get; set; }
+        public bool Occupied { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+
+    public class Reservation
+    {
+        public int ID { get; set; }
+        public int ParkingSpotID { get; set; }
+        public virtual ParkingSpot ParkingSpot { get; set; }
+        public int VehicleID { get; set; }
+        public virtual Vehicle Vehicle { get; set; }
+        public DateTime Expiry { get; set; }
+        public bool IsCurrent { get; set; }
+    }
+
+    public class ParkingContext : DbContext
+    {
+        public virtual DbSet<Vehicle> Vehicles { get; set; }
+        public virtual DbSet<Pass> Passes { get; set; }
+        public virtual DbSet<ParkingSpot> ParkingSpots { get; set; }
+        public virtual DbSet<Reservation> Reservations { get; set; }
+    }
+
+    public class ParkingHelper
+    {
+        private ParkingContext parkingContext;
+        public ParkingHelper(ParkingContext context)
+        {
+            this.parkingContext = context;
+        }
+
+        public Pass CreatePass(string purchaser, bool premium, int capacity)
+        {
+            if(purchaser.Length > 20 || purchaser.Length < 3)
+            {
+                throw new Exception("Purchaser Name should have characters between 3 and 20 only");
+            }
+            if(capacity <= 0)
+            {
+                throw new Exception("Capacity cannot be less than or equal to 0");
+            }
+            Pass newPass = new Pass();
+            newPass.Purchaser = purchaser;
+            newPass.Premium = premium;
+            newPass.Capacity = capacity;
+
+            parkingContext.Passes.Add(newPass);
+            parkingContext.SaveChanges();
+
+            return newPass;
+        }
+
+        public ParkingSpot CreateParkingSpot()
+        {
+            ParkingSpot newSpot = new ParkingSpot();
+            newSpot.Occupied = false;
+
+            parkingContext.ParkingSpots.Add(newSpot);
+            return newSpot;
+        }
+    }
+
+}
---
+++ ParkoMatikDB/ParkoMatikDBTesting/ParkingHelperTesting.cs
@@ -0,0 +1,115 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ParkoMatikDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkoMatikDBTesting
+{
+    [TestClass]
+    public class ParkingHelperTesting
+    {
+        private Mock<ParkingContext> MockParkingContext { get; set; }
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Set up fake database
+
+            Pass mockPass = new Pass() { ID = 1, Capacity= 3, Premium = false, Purchaser = "Mohid", Vehicles = null };
+
+            Vehicle mockVehicle1 = new Vehicle() { ID = 1, Parked = false, Pass = mockPass, PassID = 1,Reservations = null };
+            Vehicle mockVehicle2 = new Vehicle() { ID = 1, Parked = false, Pass = mockPass, PassID = 1, Reservations = null };
+            Vehicle mockVehicle3 = new Vehicle() { ID = 1, Parked = false, Pass = mockPass, PassID = 1, Reservations = null };
+            Vehicle mockVehicle4 = new Vehicle() { ID = 1, Parked = false, Pass = null, Reservations = null };
+
+            ParkingSpot mockParkingSpot1 = new ParkingSpot() { ID = 1, Occupied = false, Reservations = null };
+            ParkingSpot mockParkingSpot2 = new ParkingSpot() { ID = 2, Occupied = false, Reservations = null };
+
+            List<Vehicle> passVehicle = new List<Vehicle>()
+            {
+                mockVehicle1, mockVehicle2, mockVehicle3
+            };
+            mockPass.Vehicles = passVehicle;
+
+            Reservation reservation1 = new Reservation() { 
+                ID = 1, 
+                IsCurrent = true, 
+                Expiry = DateTime.Now, 
+                ParkingSpot = mockParkingSpot1, 
+                ParkingSpotID= 1, 
+                Vehicle= mockVehicle1, 
+                VehicleID= 1 };
+
+            mockVehicle1.Reservations = new List<Reservation>() { reservation1 };
+            mockVehicle1.Parked = true;
+            mockParkingSpot1.Reservations = new List<Reservation>() { reservation1 };
+            mockParkingSpot1.Occupied = true;
+
+            var AllVehicles = new List<Vehicle>()
+            {
+                mockVehicle1,
+                mockVehicle2,
+                mockVehicle3,
+                mockVehicle4
+            }.AsQueryable();
+
+            Mock<DbSet<Vehicle>> mockVehicleSet = new Mock<DbSet<Vehicle>>();
+            mockVehicleSet.As<IQueryable<Vehicle>>().Setup(m => m.Provider).Returns(AllVehicles.Provider);
+            mockVehicleSet.As<IQueryable<Vehicle>>().Setup(m => m.Expression).Returns(AllVehicles.Expression);
+            mockVehicleSet.As<IQueryable<Vehicle>>().Setup(m => m.ElementType).Returns(AllVehicles.ElementType);
+            mockVehicleSet.As<IQueryable<Vehicle>>().Setup(m => m.GetEnumerator()).Returns(AllVehicles.GetEnumerator());
+
+            var AllPasses = new List<Pass>()
+            {
+                mockPass
+            }.AsQueryable();
+
+            Mock<DbSet<Pass>> mockPassSet = new Mock<DbSet<Pass>>();
+            mockPassSet.As<IQueryable<Pass>>().Setup(m => m.Provider).Returns(AllPasses.Provider);
+            mockPassSet.As<IQueryable<Pass>>().Setup(m => m.Expression).Returns(AllPasses.Expression);
+            mockPassSet.As<IQueryable<Pass>>().Setup(m => m.ElementType).Returns(AllPasses.ElementType);
+            mockPassSet.As<IQueryable<Pass>>().Setup(m => m.GetEnumerator()).Returns(AllPasses.GetEnumerator());
+
+
+            MockParkingContext = new Mock<ParkingContext>();
+
+            MockParkingContext.Setup(context => context.Vehicles).Returns(mockVehicleSet.Object);
+            MockParkingContext.Setup(context => context.Passes).Returns(mockPassSet.Object);
+
+
+            //If there are no “Current” Reservation relationships on the ParkingSpot and Vehicle, the system creates a new “Current” one.
+            //Both the Vehicle and ParkingSpot have their Parked properties set as True.
+        }
+        [TestMethod]
+        public void CreatePassThrowsExceptionIfPurchaserIsNotTheCorrectStringLength()
+        {
+            //Arrange
+            ParkingHelper helper = new ParkingHelper(MockParkingContext.Object);
+
+            //Act and Assert
+            Assert.ThrowsException<Exception>(() =>
+            helper.CreatePass("a", false, 2)); //less than 3
+
+            Assert.ThrowsException<Exception>(() =>
+            helper.CreatePass("osdkdsbfjskdfkbsdfkjsdbfjbdsfbsdkfbjs", false, 2)); //more than 20
+
+        }
+        [TestMethod]
+        public void CreatePassThrowsExceptionIfCapacityIsZero()
+        {
+            //Arrange
+            ParkingHelper helper = new ParkingHelper(MockParkingContext.Object);
+
+            //Act and Assert
+            Assert.ThrowsException<Exception>(() =>
+            helper.CreatePass("aman", false, 0)); //capacity is zero
+
+            Assert.ThrowsException<Exception>(() =>
+            helper.CreatePass("aman", false, -10)); //capacity is zero
+
+        }
+
+    }
+}
